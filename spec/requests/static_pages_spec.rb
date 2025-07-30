b8fb7c95cb72f2890082cdec60a89bbb51b60e8d
Bailey Beck <baileybeck@Mac.lan>
2025-07-30 11:45:09 -0400

    add contact page and spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,4 +24,12 @@ RSpec.describe "StaticPages", type: :request do
       expect(response.body).to include("<title>About</title>")
     end
   end
+
+  describe "GET /contact" do
+    it "returns http success" do
+      get "/contact"
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include ("<title>Contact</title")
+    end
+  end
 end
